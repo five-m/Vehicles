f6ddd6d98590f8ebbba1d15604790cfe454ecf38
Mr.TnT <wajihhalwani79@gmail.com>
2019-12-18 13:06:41 +0300

    Add FiveM Resources line

--- BMW/2016 BMW X6M 1.1/__resource.lua
+++ BMW/2016 BMW X6M 1.1/__resource.lua
@@ -13,3 +13,4 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+-- FiveM Resources
