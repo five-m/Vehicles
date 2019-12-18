8aa6b0978adfb57ecdb47d6673fcb143fbbd2a68
Mr.TnT <wajihhalwani79@gmail.com>
2019-12-18 13:10:16 +0300

    Remove FiveM resoures Cars line

--- BMW/2016 BMW X6M 1.1/__resource.lua
+++ BMW/2016 BMW X6M 1.1/__resource.lua
@@ -13,4 +13,3 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
--- FiveM Resources
